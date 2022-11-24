e84c36f65e233c0de4737772c5176b88c8884bd4
PierreBeur <cbeurtheret@ucsd.edu>
2022-11-23 17:21:53 -0800

    Add Buttons for Console Testing

--- main.js
+++ main.js
@@ -10,3 +10,32 @@ form.addEventListener('submit', e => {
 
 // Buttons for Console Testing
 const errorBtns = document.querySelectorAll('#error-btns > button');
+const x = 2;
+const y = 3;
+const reason = 'x is expected to be equal to y';
+const table = [
+  {code: 'CSE 120', title: ' Principles of Computer Operating Systems'},
+  {code: 'ECE 111', title: 'Advanced Digital Design Project'},
+  {code: 'CSE 167', title: 'Computer Graphics'},
+  {code: 'ECE 140A', title: 'The Art of Product Engineering I'}
+];
+const clickFunctions = {
+  'Console Log':            () => console.log('Console Log'),
+  'Console Error':          () => console.error('Console Error'),
+  'Console Count':          () => console.count('Console Count'),
+  'Console Warn':           () => console.warn('Console Warn'),
+  'Console Assert':         () => console.assert(x == y, {x, y, reason}),
+  'Console Clear':          () => console.clear(),
+  'Console Dir':            () => console.dir(clickFunctions),
+  'Console dirxml':         () => console.dirxml(document),
+  'Console Group Start':    () => console.group('Console Group Start'),
+  'Console Group End':      () => console.groupEnd('Console Group Start'),
+  'Console Table':          () => console.table(table),
+  'Start Timer':            () => console.time('Start Timer'),
+  'End Timer':              () => console.timeEnd('Start Timer'),
+  'Console Trace':          () => console.trace(),
+  'Trigger a Global Error': () => boom()
+}
+for (const button of errorBtns) {
+  button.addEventListener('click', clickFunctions[button.innerText]);
+}
