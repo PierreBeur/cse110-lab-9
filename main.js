842e3111c059b8085fd936bf96573cf5cd2aec4a
PierreBeur <cbeurtheret@ucsd.edu>
2022-11-23 17:20:43 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-let form = document.querySelector('form');
+const form = document.querySelector('form');
 form.addEventListener('submit', e => {
   e.preventDefault();
-  let output = document.querySelector('output');
-  let firstNum = document.querySelector('#first-num').value;
-  let secondNum = document.querySelector('#second-num').value;
-  let operator = document.querySelector('#operator').value;
+  const output = document.querySelector('output');
+  const firstNum = document.querySelector('#first-num').value;
+  const secondNum = document.querySelector('#second-num').value;
+  const operator = document.querySelector('#operator').value;
   output.innerHTML = eval(`${firstNum} ${operator} ${secondNum}`);
 });
 
-let errorBtns = Array.from(document.querySelectorAll('#error-btns > button'));
-
-// Start your code here
-// You may move this JS to another file if you wish
+// Buttons for Console Testing
+const errorBtns = document.querySelectorAll('#error-btns > button');
